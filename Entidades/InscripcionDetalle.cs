0eb3d2388f814588e4aa15f1259c20cf15ed5d4a
Adm <Adm@DESKTOP-E9RC5UA>
2019-06-25 22:04:14 -0400

    Contexto

---
+++ Entidades/InscripcionDetalle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroUniversitario.Entidades
+{
+ public class InscripcionDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int AsignaturaId { get; set; }
+        public int EstudianteId { get; set; }
+
+        //Faltan atributos
+
+        public InscripcionDetalle()
+        {
+            Id = 0;
+            AsignaturaId = 0;
+            EstudianteId = 0;
+        }
+        public InscripcionDetalle(int Id, int AsignaturaId, int Estudiante)
+        {
+            this.Id = Id;
+            this.AsignaturaId = AsignaturaId;
+            this.EstudianteId = EstudianteId;
+        }
+    }
+}
